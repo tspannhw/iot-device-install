f645475b1e6a88efde1873d49673caf7b6495048
Timothy Spann <tspann@hortonworks.com>
2019-06-03 15:25:17 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+pip3.6 install easydict -U
+pip3.6 install scikit-learn -U
+pip3.6 install opencv-python -U --user
+pip3.6 install numpy -U
+pip3.6 install mxnet -U
+pip3.6 install mxnet-mkl -U
+pip3.6 install gluoncv --upgrade
